162a4f86a52104dabb442a2806588a7a066922df
Enger Colman <engerterseer@gmail.com>
2020-04-14 00:48:49 +0100

    Initial dist subtree commit

---
+++ precache-manifest.556b12155b016ac487e544b1a0d33469.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d7c785b79a116285b8f",
+    "url": "covid19-ui/css/app.b543433f.css"
+  },
+  {
+    "revision": "7a10563bbde3097d1a6f4b4fdd22016f",
+    "url": "covid19-ui/index.html"
+  },
+  {
+    "revision": "6d7c785b79a116285b8f",
+    "url": "covid19-ui/js/app.e3386f9d.js"
+  },
+  {
+    "revision": "0532dc04a348f6eaa5ac",
+    "url": "covid19-ui/js/chunk-vendors.a2bfe163.js"
+  },
+  {
+    "revision": "6b3ce90e5f19e18664283e9277f759b1",
+    "url": "covid19-ui/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "covid19-ui/robots.txt"
+  }
+]);
